0d108d3f509d7f3dd8db6712efd06dea2b706a2a
Garp <163739965+deckeym@users.noreply.github.com>
2024-06-21 11:23:51 +0700

    Update allFunc.cpp

--- AES/allFunc.cpp
+++ AES/allFunc.cpp
@@ -8,25 +8,22 @@
 
 using namespace std;
 
-// Функция для генерации ключа
 void keyGen(vector<unsigned char>& key) {
-    minstd_rand gen(random_device{}()); // Генератор случайных чисел
-    uniform_int_distribution<int> numbers(0, 9); // Распределение случайных чисел от 0 до 9 (кодировка ASCII)
+    minstd_rand gen(random_device{}());
+    uniform_int_distribution<int> numbers(0, 9); //кодировка ASCII
     for (int i = 1; i <= 16; i++) {
-        key.push_back('0' + numbers(gen)); // Добавляем случайный символ в ключ
+        key.push_back('0' + numbers(gen));
     }
 }
 
-// Функция для выполнения побитового сложения состояния с раундовым ключом
-vector<unsigned char> AddRoundKey(vector<unsigned char>& first, vector<unsigned char>& second) {
+vector<unsigned char> AddRoundKey(vector<unsigned char>& first, vector<unsigned char>& second) { // выполняем побитовое сложение состояния с раундовым ключом
     vector<unsigned char> temp;
     for (int i = 0; i < 4; ++i) {
-        temp.push_back(first[i] ^ second[i]); // XOR операция для каждого байта
-    }
+        temp.push_back(first[i] ^ second[i]);
+    } // проходимся по столбцу
     return temp;
 }
 
-// Функция для выполнения подстановки байтов с использованием S-box
 void SubBytes(vector<unsigned char>& state) {
     vector<unsigned char> Sbox = {
        0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
@@ -49,29 +46,27 @@ void SubBytes(vector<unsigned char>& state) {
 
     vector<unsigned char> temp;
     for (auto i : state) {
-        temp.push_back(Sbox[i]); // Замена байта на соответствующее значение из S-box
+        temp.push_back(Sbox[i]);
     }
     state = temp;
 }
 
-// Функция для выполнения сдвига строк
 void ShiftRows(vector<unsigned char>& state) {
     vector<unsigned char> temp(state.size());
     for (int i = 1; i < state.size(); i++) {
-        temp[i - 1] = state[i]; // Сдвиг элементов влево
+        temp[i - 1] = state[i]; //сдвиг элементов влево
     }
-    temp[state.size() - 1] = state[0]; // Последний элемент становится первым
+    temp[state.size() - 1] = state[0]; //последний элемент становится первым
 
     state = temp;
 }
 
-// Функция для умножения двух байтов в поле Галуа
-unsigned char gmul(unsigned char a, unsigned char b) {
+unsigned char gmul(unsigned char a, unsigned char b) { // умножение двух байтов в поле Галуа
     unsigned char result = 0;
     unsigned char hi_bit_set;
     for (unsigned char i = 0; i < 8; i++) {
         if (b & 1) { // Если младший бит b равен 1
-            result ^= a; // Выполняем XOR текущего результата с переменной a
+            result ^= a; //Выполняем XOR текущего результата с переменной a
         }
         hi_bit_set = a & 0x80; // Проверяем старший бит переменной a перед сдвигом
         a <<= 1; // Сдвигаем переменную a влево на 1 бит
@@ -83,74 +78,242 @@ unsigned char gmul(unsigned char a, unsigned char b) {
     return result;
 }
 
-// Генерация таблицы умножения на 2
 vector<unsigned char> generate_mult_by_2() {
     vector<unsigned char> mult_by_2(256);
     for (int i = 0; i < 256; ++i) {
-        mult_by_2[i] = gmul(i, 2); // Умножаем каждый байт на 2
+        mult_by_2[i] = gmul(i, 2);
     }
     return mult_by_2;
 }
 
-// Генерация таблицы умножения на 3
+// Генерация таблицы mult_by_3
 vector<unsigned char> generate_mult_by_3() {
     vector<unsigned char> mult_by_3(256);
     for (int i = 0; i < 256; ++i) {
-        mult_by_3[i] = gmul(i, 3); // Умножаем каждый байт на 3
+        mult_by_3[i] = gmul(i, 3);
     }
     return mult_by_3;
 }
 
-// Функция для выполнения операции MixColumns
+vector<unsigned char> generate_mult_by_14() {
+    vector<unsigned char> mult_by_14(256);
+    for (int i = 0; i < 256; ++i) {
+        mult_by_14[i] = gmul(i, 14);
+    }
+    return mult_by_14;
+}
+
+vector<unsigned char> generate_mult_by_9() {
+    vector<unsigned char> mult_by_9(256);
+    for (int i = 0; i < 256; ++i) {
+        mult_by_9[i] = gmul(i, 9);
+    }
+    return mult_by_9;
+}
+
+vector<unsigned char> generate_mult_by_13() {
+    vector<unsigned char> mult_by_13(256);
+    for (int i = 0; i < 256; ++i) {
+        mult_by_13[i] = gmul(i, 13);
+    }
+    return mult_by_13;
+}
+
+vector<unsigned char> generate_mult_by_11() {
+    vector<unsigned char> mult_by_11(256);
+    for (int i = 0; i < 256; ++i) {
+        mult_by_11[i] = gmul(i, 11);
+    }
+    return mult_by_11;
+}
+
 void MixColumns(vector<unsigned char>& state) {
-    vector<unsigned char> mult_by_2 = generate_mult_by_2();
+    vector<unsigned char> mult_by_2 = generate_mult_by_2(); //операции умножения на 2 и 3 к столбцам матрицы данных
+
     vector<unsigned char> mult_by_3 = generate_mult_by_3();
 
-    vector<unsigned char> temp(state.size());
-    temp[0] = mult_by_2[state[0]] ^ mult_by_3[state[1]] ^ state[2] ^ state[3];
-    temp[1] = state[0] ^ mult_by_2[state[1]] ^ mult_by_3[state[2]] ^ state[3];
-    temp[2] = state[0] ^ state[1] ^ mult_by_2[state[2]] ^ mult_by_3[state[3]];
-    temp[3] = mult_by_3[state[0]] ^ state[1] ^ state[2] ^ mult_by_2[state[3]];
-
+    vector<unsigned char> temp;
+    temp.push_back(mult_by_2[state[0]] ^ mult_by_3[state[1]] ^ state[2] ^ state[3]);
+    temp.push_back(mult_by_2[state[1]] ^ mult_by_3[state[2]] ^ state[0] ^ state[3]);
+    temp.push_back(mult_by_2[state[2]] ^ mult_by_3[state[3]] ^ state[0] ^ state[1]);
+    temp.push_back(mult_by_2[state[3]] ^ mult_by_3[state[0]] ^ state[1] ^ state[2]);
     state = temp;
 }
 
-// Функция шифрования данных
-void encrypt(vector<unsigned char>& data, vector<unsigned char>& key) {
-    vector<unsigned char> state = AddRoundKey(data, key); // Начальная операция AddRoundKey
+void KeyExpansion(vector<unsigned char>& key, vector<vector<unsigned char>>& roundKeys) {
+    const vector<unsigned char> Rcon = {
+        0x00, 0x00, 0x00, 0x00,
+        0x01, 0x00, 0x00, 0x00,
+        0x02, 0x00, 0x00, 0x00,
+        0x04, 0x00, 0x00, 0x00,
+        0x08, 0x00, 0x00, 0x00,
+        0x10, 0x00, 0x00, 0x00,
+        0x20, 0x00, 0x00, 0x00,
+        0x40, 0x00, 0x00, 0x00,
+        0x80, 0x00, 0x00, 0x00,
+        0x1b, 0x00, 0x00, 0x00,
+        0x36, 0x00, 0x00, 0x00
+    };
 
-    for (int i = 0; i < 9; ++i) {
-        SubBytes(state); // Замена байтов
-        ShiftRows(state); // Сдвиг строк
-        MixColumns(state); // Перемешивание столбцов
-        state = AddRoundKey(state, key); // Добавление раундового ключа
+    vector<unsigned char> temp;
+    int i = 0;
+    int Nk = 4;
+    int Nb = 4;
+    int Nr = 10;
+
+    roundKeys.resize(Nb * (Nr + 1), vector<unsigned char>(4));
+
+    while (i < Nk) {
+        temp = { key[4 * i], key[4 * i + 1], key[4 * i + 2], key[4 * i + 3] };
+        roundKeys[i] = temp;
+        i++;
     }
 
-    SubBytes(state); // Замена байтов в последнем раунде
-    ShiftRows(state); // Сдвиг строк
-    state = AddRoundKey(state, key); // Последняя операция AddRoundKey
-
-    data = state;
+    i = Nk;
+    while (i < (Nb * (Nr + 1))) {
+        temp = roundKeys[i - 1];
+        if (i % Nk == 4) {
+            ShiftRows(temp);
+            SubBytes(temp);
+            for (int k = 0; k < temp.size(); ++k) {
+                temp[k] = temp[k] ^ Rcon[i / Nk];
+            }
+        }
+        else if (Nk > 6 && (i % Nk == 4)) {
+            SubBytes(temp);
+        }
+        for (int j = 0; j < temp.size(); ++j) {
+            roundKeys[i][j] = roundKeys[i - Nk][j] ^ temp[j];
+        }
+        i++;
+    }
 }
 
-int main() {
-    vector<unsigned char> data = { 0x32, 0x88, 0x31, 0xe0 }; // Пример данных для шифрования
-    vector<unsigned char> key;
-    keyGen(key); // Генерация ключа
-
-    cout << "Original data: ";
-    for (auto d : data) {
-        cout << hex << setw(2) << setfill('0') << static_cast<int>(d) << " ";
+void InvShiftRows(vector<unsigned char>& state) {
+    vector<unsigned char> temp(state.size());
+    for (int i = 0; i < state.size(); i++) {
+        temp[i] = state[(i - (i % 4) * (i % 4)) % state.size()];
     }
-    cout << endl;
+    state = temp;
+}
 
-    encrypt(data, key); // Шифрование данных
-
-    cout << "Encrypted data: ";
-    for (auto d : data) {
-        cout << hex << setw(2) << setfill('0') << static_cast<int>(d) << " ";
+void InvSubBytes(vector<unsigned char>& state) {
+    vector<unsigned char> InvSbox = {
+        0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+        0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+        0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+        0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+        0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+        0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+        0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+        0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+        0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+        0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+        0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+        0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+        0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+        0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+        0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+        0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+    };
+    vector<unsigned char> temp;
+    for (auto i : state) { // пройдемся по каждому элементу строки и заменим его на табличное значение
+        temp.push_back(InvSbox[i]);
     }
-    cout << endl;
+    state = temp;
+}
 
-    return 0;
+void InvMixColumns(vector<unsigned char>& state) {
+    vector<unsigned char> mult_by_14 = generate_mult_by_14();
+    vector<unsigned char> mult_by_9 = generate_mult_by_9();
+    vector<unsigned char> mult_by_13 = generate_mult_by_13();
+    vector<unsigned char> mult_by_11 = generate_mult_by_11();
+
+    vector<unsigned char> temp;
+    temp.push_back(mult_by_14[state[0]] ^ mult_by_9[state[1]] ^ mult_by_13[state[2]] ^ mult_by_11[state[3]]);
+    temp.push_back(mult_by_14[state[1]] ^ mult_by_9[state[2]] ^ mult_by_13[state[3]] ^ mult_by_11[state[0]]);
+    temp.push_back(mult_by_14[state[2]] ^ mult_by_9[state[3]] ^ mult_by_13[state[0]] ^ mult_by_11[state[1]]);
+    temp.push_back(mult_by_14[state[3]] ^ mult_by_9[state[0]] ^ mult_by_13[state[1]] ^ mult_by_11[state[2]]);
+    state = temp;
+}
+
+void blocksGenerator(string& text, vector<vector<vector<unsigned char>>>& block) {
+    while (text.size() % 16 != 0) { // добавление пробелов
+        text += ' ';
+    }
+
+    vector<vector<vector<unsigned char>>> v; // временный block
+    vector<vector<unsigned char>> sixteen(4, vector<unsigned char>(4)); // матрица 4
+
+    for (int i = 0; i < text.size(); ++i) {
+        int a = (i % 16) % 4; // по блокам выбирается строка
+        int b = (i % 16) / 4;// по блокам выбирается столбец
+        sixteen[a][b] = text[i];
+
+        if ((i + 1) % 16 == 0) { //если блок заполнился
+            v.push_back(sixteen);
+            sixteen = vector<vector<unsigned char>>(4, vector<unsigned char>(4));
+        }
+    }
+
+    block = v;
+}
+
+vector<vector<unsigned char>> AESencrypt(vector<vector<unsigned char>>& block, vector<vector<unsigned char>>& roundKeys, vector<unsigned char> key) {
+
+    vector<vector<unsigned char>> temp(4, vector<unsigned char>(4, 0)); 
+    for (int i = 0; i < 4; ++i) {
+        temp[i] = AddRoundKey(block[i], roundKeys[i]);
+    }
+    
+    for (int i = 1; i <= 9; i++) { 
+        for (int j = 0; j <= 3; j++) { 
+            SubBytes(temp[j]);
+            ShiftRows(temp[j]);
+            MixColumns(temp[j]);
+            temp[j] = AddRoundKey(temp[j], roundKeys[i]);
+        }
+    }
+    
+    for (int j = 0; j <= 3; j++) {
+        SubBytes(temp[j]);
+        ShiftRows(temp[j]);
+        temp[j] = AddRoundKey(temp[j], roundKeys[10]);
+    }
+
+    return temp;
+}
+
+vector<vector<unsigned char>> AESdecrypt(vector<vector<unsigned char>>& block, vector<vector<unsigned char>>& roundKeys, vector<unsigned char> key) {
+    // Расширение ключа
+    vector<vector<unsigned char>> roundKeysDec(44, vector<unsigned char>(4));
+    KeyExpansion(key, roundKeysDec);
+
+    vector<vector<unsigned char>> v(4, vector<unsigned char>(4, 0));
+
+    // Первый этап - AddRoundKey с последним раундовым ключом
+    for (int j = 0; j <= 3; j++) {
+        v[j] = AddRoundKey(block[j], roundKeys[10]);
+    }
+
+    // Циклический этап - InvShiftRows, InvSubBytes, AddRoundKey, InvMixColumns
+    for (int i = 9; i >= 1; i--) {
+        for (int j = 0; j <= 3; j++) {
+            v[j] = AddRoundKey(v[j], roundKeys[i]);
+            InvMixColumns(v[j]);
+            InvShiftRows(v[j]);
+            InvSubBytes(v[j]);
+        }
+    }
+
+    // Последний этап - InvShiftRows, InvSubBytes, AddRoundKey
+    for (int j = 0; j <= 3; j++) {
+        v[j] = AddRoundKey(v[j], roundKeys[0]);
+    }
+    for (int j = 0; j <= 3; j++) {
+        InvShiftRows(v[j]);
+        InvSubBytes(v[j]);
+    }
+
+    return v;
 }
